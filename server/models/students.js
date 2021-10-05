b7a495089ed6c8e25a0c1c7498b6272ba05364b4
avdoyizode <88720183+avdoyizode@users.noreply.github.com>
2021-10-05 17:38:29 +0530

    bug fix

--- server/models/students.js
+++ server/models/students.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-    name: String,
-    age: String,
-    test1: Number,
-    classId: String
-    
+  name: String,
+  age: String,
+  test1: String,
+  classId: String,
 });
 
-module.exports = mongoose.model('Student', studentSchema);
+module.exports = mongoose.model("Student", studentSchema);
