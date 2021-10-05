211f1482be56fb3e4109a77e8b66282b4acf14f3
avdoyizode <88720183+avdoyizode@users.noreply.github.com>
2021-10-05 17:36:18 +0530

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,15 @@
 import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-
 
 import StudentList from "./Components/StudentList";
 import AddStudent from "./Components/AddStudent";
 
-//Apollo client setup
-const client = new ApolloClient({
-  uri: 'http://localhost:4001/graphql',
-  cache: new InMemoryCache()
-})
-
-
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div id="main">
-        <h1>Student Tracker</h1>
-        <StudentList />
-        <AddStudent />
-      </div>
-    </ApolloProvider>
+    <div id="main">
+      <h1>Student Tracker</h1>
+      <StudentList />
+      <AddStudent />
+    </div>
   );
 }
 
